18c8e8272380095e88f9f1ba084364accfdf2a62
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2023-09-20 12:19:06 +0000

    fix: use https url

--- setup-smart-contracts.sh
+++ setup-smart-contracts.sh
@@ -1,4 +1,4 @@
-git clone git@github.com:WeatherXM/smart-contracts.git
+git clone https://github.com/WeatherXM/smart-contracts.git
 cd smart-contracts
 git checkout develop
 git submodule update --init --recursive
